36f23539204d40b12224e7fedaa95811db728e4f
dunewalkin <sergei.gunchenko@gmail.com>
2024-03-26 16:22:32 +0400

    add range

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import './questions.scss'
+import React, { useState, useEffect } from 'react';
+import './questions.scss';
 
 const Questions = ({ selectedQuiz, currentQuestionIndex }) => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const calculateProgress = () => {
+      const newProgress = ((currentQuestionIndex + 1) / selectedQuiz.questions.length) * 100;
+      setProgress(newProgress);
+    };
+
+    calculateProgress();
+  }, [currentQuestionIndex, selectedQuiz.questions.length]);
+
+  const inputStyle = {
+   width: `${progress}%`
+  };
+
   return (
     <div>
       {selectedQuiz && (
-         <div className='question-section'>
-            <p className='body-s '>{`Question ${currentQuestionIndex + 1} of ${selectedQuiz.questions.length}`}</p>
+        <div className='question-section'>
+            <p className='body-s'>{`Question ${currentQuestionIndex + 1} of ${selectedQuiz.questions.length}`}</p>
             <h1 className='heading-s'>{selectedQuiz.questions[currentQuestionIndex].question}</h1>
 
-            <input 
-            type="range" 
-            min="1" 
-            max={selectedQuiz.questions.length} 
-            value={currentQuestionIndex + 1} 
-          />
-         </div>       
+            <div className='range-wrapper'>
+               <div 
+                  className='range-progress'
+                  style={inputStyle}>
+               </div>
+            </div>
+         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Questions
+export default Questions;
